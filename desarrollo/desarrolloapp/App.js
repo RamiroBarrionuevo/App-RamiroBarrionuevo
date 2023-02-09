b82a57f09b29dfc105068e9326be17a568190212
Ramiro Barrionuevo <ramidbb@hotmail.com.ar>
2023-02-09 20:40:41 -0300

    desafio

--- desarrollo/desarrolloapp/App.js
+++ desarrollo/desarrolloapp/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola mundo</Text>
+      <Text>¡Hola mundo!</Text>
+      <Text> Soy Ramiro Barrionuevo</Text>
       <StatusBar style="auto" />
     </View>
   );
